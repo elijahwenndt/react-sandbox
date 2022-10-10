4675c1d0924ee6b55b7265d322aec5ad615febce
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-10-10 20:10:41 +0000

    sandbox stuff

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react'
+
+function MyButton() {
+  const [count, setCount] = useState(0)
+  function handleClick(){
+    setCount(count + 1)
+    // alert('you clicked this shit');
+  };
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  )
+}
+function AboutPage() {
+  return (
+  <div>
+    <h1>i am testing this</h1>
+    <p>hello there. <br /> i am testing some shit out</p>
+  </div>
+  )
+}
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div>
+        <h1>counters update separetely</h1>
+        <MyButton /><br />
+        <MyButton />
+      </div>
+      <AboutPage />
+      <Profile />
+      <ShoppingList />
+    </div>
+  );
+}
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+function ShoppingList () {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+export default App;
